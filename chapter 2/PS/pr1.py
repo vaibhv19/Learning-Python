efaa243e713299eb80757a2d38b1b2265c07e4b3
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2025-03-15 19:48:42 +0530

    day 1

---
+++ chapter 2/PS/pr1.py
@@ -0,0 +1,6 @@
+#Write a python program to add two numbers
+a = 1
+
+b = 5
+
+print(a + b)
---
+++ chapter 2/PS/pr6.py
@@ -0,0 +1,8 @@
+#Write a python program to calculate the square of a number entered by the user.
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
